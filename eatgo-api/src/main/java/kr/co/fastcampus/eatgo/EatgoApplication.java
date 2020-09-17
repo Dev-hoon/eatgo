3218eaf76bd49b83d9d554ec0020c21ddf25d3e4
Kimtaehun <asdnm5030@gmail.com>
2020-09-17 15:32:02 +0900

    sprint boot  생성 및interfaces 생성

---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatgoApplication.class, args);
+	}
+
+}
---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,12 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello worlds!!!";
+    }
+}
